91f569a6a6006f8f04d83de8b6d4d45e1bb72651
SophiaLangheld <gaoxuefei55@gmail.com>
2021-07-21 13:59:37 +0200

    promise

--- promise-test/index.js
+++ promise-test/index.js
@@ -2,7 +2,7 @@ const fs = require('fs')
 const path = require('path')
 
 //callback
-function getFileContent(fileName, callback){
+/*function getFileContent(fileName, callback){
     const fullFileName = path.resolve( __dirname, 'files', fileName)
     fs.readFile(fullFileName, (err,data) => {
         if(err){
@@ -24,5 +24,32 @@ getFileContent('a.json', aData => {
             console.log('c data', cData)
         })
     })
+})*/
+
+//promise
+function getFileContent(fileName){
+    const promise = new Promise((resolve, reject) => {
+        const fullFileName = path.resolve(__dirname, 'files', fileName)
+        fs.readFile(fullFileName, (err, data) =>{
+            if(err){
+                reject(err)
+                return
+            }
+            resolve(
+                JSON.parse(data.toString())
+            )
+        })
+    })
+    return promise
+}
+
+getFileContent('a.json').then(aData =>{
+    console.log('a data', aData)
+    return getFileContent(aData.next)
+}).then(bData =>{
+    console.log('b data', bData)
+    return getFileContent(bData.next)
+}).then(cData =>{
+    console.log('c data', cData)
 })
 
