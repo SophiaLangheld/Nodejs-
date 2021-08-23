a96fe3e527daf464f6fd7a7a1c6a6d25bb6f6752
SophiaLangheld <gaoxuefei55@gmail.com>
2021-08-23 11:52:55 +0200

    密码加密

--- blog-1/src/controller/user.js
+++ blog-1/src/controller/user.js
@@ -1,4 +1,5 @@
 const {exec, escape} = require('../db/mysql')
+const { genPassword } = require('../utils/cryp')
 
 const login = (username, password) => {
     //先使用假数据
@@ -7,12 +8,18 @@ const login = (username, password) => {
     }
     return false*/
     username = escape(username)
+    
+
+    // 生成加密密码
+    password = genPassword(password)
+
     password = escape(password)
+
     const sql = `
         select username, realname from users where username=${username} and password=${password};
     `
     console.log('sql is', sql)
-    
+
     return exec(sql).then(rows => {
         return rows[0] || {}
     })
