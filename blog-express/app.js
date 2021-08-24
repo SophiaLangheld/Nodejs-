431bc15b2181fe7b0ff8aa6326ccacf0a3c8fa69
SophiaLangheld <gaoxuefei55@gmail.com>
2021-08-24 10:04:54 +0200

    初始化环境 mysql xss  , add express-session

--- blog-express/app.js
+++ blog-express/app.js
@@ -3,26 +3,38 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const session = require('express-session')
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+//var indexRouter = require('./routes/index');
+//var usersRouter = require('./routes/users');
 const blogRouter = require('./routes/blog');
 const userRouter = require('./routes/user');
 
 var app = express();
 
+/*
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+*/
 
 app.use(logger('dev'));  //日志
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public'))); //静态文件
+//app.use(express.static(path.join(__dirname, 'public'))); //静态文件
 
-app.use('/', indexRouter);      //注册
-app.use('/users', usersRouter); //注册
+app.use(session({
+  secret: 'WJiol#23123_',
+  cookie:{
+    //path: '/', //默认配置
+    // httpOnly: true, //默认配置
+    maxAge: 24 * 60 * 60 * 1000
+  }
+}))
+
+//app.use('/', indexRouter);      //注册
+//app.use('/users', usersRouter); //注册
 app.use('/api/blog', blogRouter);
 app.use('/api/user', userRouter);
 
--- blog-express/routes/blog.js
+++ blog-express/routes/blog.js
@@ -1,11 +1,28 @@
 var express = require('express');
 var router = express.Router();
+const {
+    getList,
+    getDetail,
+    newBlog,
+    updateBlog,
+    delBlog
+} = require('../controller/blog')
+const { SuccessModel, ErrorModel} = require('../model/resModel')
 
 router.get('/list', function(req, res, next){
-    res.json({
+    let author = req.query.author || ''
+    const keyword = req.query.keyword || ''
+
+    const result = getList(author, keyword)
+    return result.then(listData =>{
+        res.json(
+            new SuccessModel(listData)
+        )
+    })
+    /*res.json({
         errno: 0,
         data: [1,2,3]
-    })
+    })*/
 });
 
 router.get('/detail', function(req, res, next){
